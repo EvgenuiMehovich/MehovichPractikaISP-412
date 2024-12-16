98ee49720651ab8076c951d7c7c056c3571ac41f
evgen <evgenijmehovic@gmail.com>
2024-12-16 22:32:11 +0400

    Обработчики пустого логина и пароля при авторизации

--- MehovichPractikaISP/MehovichPractikaISP/MainPage.xaml.cs
+++ MehovichPractikaISP/MehovichPractikaISP/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+
 namespace MehovichPractikaISP
 {
     public partial class MainPage : ContentPage
@@ -20,6 +21,23 @@ namespace MehovichPractikaISP
         private void Exit_Clicked(object sender, EventArgs e)
         {
             System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+       
+
+        private async void Avtor_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Login.Text))
+            {
+                await DisplayAlert("Ошибка", "Логин не может быть пустым", "OK");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(Password.Text))
+            {
+                await DisplayAlert("Ошибка", "Пароль не может быть пустым", "OK");
+                return;
+            }
         }
     }
 }
